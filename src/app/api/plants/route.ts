9c61c857340fcf3315581c90d1b72a0688bebfa1
Busra Temizyurek <busratemizyureek@gmail.com>
2025-10-31 16:24:53 -0700

    auth is implemented in a temporary way

--- src/app/api/plants/route.ts
+++ src/app/api/plants/route.ts
@@ -1,8 +1,36 @@
 import { Plant } from "@/models/plants";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { verifyToken } from "@clerk/nextjs/server";
 
-export async function GET(): Promise<NextResponse<Plant[]>> {
-  const plants = await Plant.findAll();
+export async function GET(
+  req: NextRequest
+): Promise<NextResponse<Plant[] | { error: string }>> {
+  const authHeader = req.headers.get("authorization");
 
-  return NextResponse.json(plants);
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    // return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    return NextResponse.json(await Plant.findAll());
+  }
+
+  const token = authHeader.substring(7);
+
+  try {
+    const verified = await verifyToken(token, {
+      secretKey: process.env.CLERK_SECRET_KEY!,
+    });
+    const userId = verified.sub;
+
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const plants = await Plant.findAll({
+      where: { userId },
+    });
+
+    return NextResponse.json(plants);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Invalid token" }, { status: 401 });
+  }
 }
