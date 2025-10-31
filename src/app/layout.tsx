cee5407084907d1add9f45df8bbcc0705147ad79
Busra Temizyurek <busratemizyureek@gmail.com>
2025-10-31 16:41:48 -0700

    metadata fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,12 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Thirsty Plant",
   description: "Moisture amount tracker for your plant",
-  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
 };
 
 export default function RootLayout({
